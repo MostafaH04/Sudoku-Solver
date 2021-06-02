3d422a98437c55406b5a9a3e70d3c8657c9cadbf
Mostafa Hussein <tnt.empire00@gmail.com>
2021-06-02 11:57:33 -0400

    input

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import solve
 
-gridExample = [
+grid = [
     [0,7,0,0,2,0,0,4,6],
     [0,6,0,0,0,0,8,9,0],
     [2,0,0,8,0,0,7,1,5],
@@ -12,4 +12,34 @@ gridExample = [
     [4,3,0,0,8,0,0,7,0]    
 ]
 
-solve.solve(gridExample)
+solve.show(grid)
+
+
+continueQ = input("Do you want to make ur own?")
+if continueQ.lower() in ['yes','y']:
+    for y in range(9):
+        for x in range(9):
+            grid[y][x] = 0
+
+    for y in range(9):
+        for x in range(9):
+            while True:
+                solve.show(grid)
+                inNum = input(f"pick number for ({x},{y})")
+                
+                try:
+                    inNum = int(inNum)
+                    if inNum < 10 and inNum >= 0:
+                        if solve.possible(y,x, inNum, grid) != True:
+                            raise TypeError
+                        grid[y][x] = inNum
+                    else:
+                        raise TypeError
+                    
+                    break
+                
+                except:
+                    print("Incorrect Format Try Again\n")
+
+
+solve.solve(grid)
